fe2c59aabd7c5f7e8a811ddef50edc6029ba370d
priyanshitiwari123 <84151353+priyanshitiwari123@users.noreply.github.com>
2021-05-14 00:45:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "welcome" && b=="14-05-2021")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
